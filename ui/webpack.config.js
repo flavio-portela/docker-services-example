a7deb2b79308ec5297c63cec817e98d187dff173
Flavio Portela Valenzuela <fportela@tiempodevelopment.com>
2017-09-19 11:15:08 -0700

    added babel

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
+    watch: true,
+    watchOptions: {
+        poll: 300
+    },
     entry: './src/js/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/}
+        ]
     }
 };
